d2f87d72b32fc5b19b8dc37d2d8bb7f96871ad01
AWEHA Nicolas <awenico98@gmail.com>
2024-10-15 11:32:03 +0100

    feat: server initiation

--- src/server.js
+++ src/server.js
@@ -1,2 +1,8 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on http://localhost:${process.env.PORT}`)
+);
