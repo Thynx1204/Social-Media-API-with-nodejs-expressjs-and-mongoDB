eda95dfceb7ff7491a8b9ce2cba2132ae14e1055
AWEHA Nicolas <awenico98@gmail.com>
2024-10-15 18:11:56 +0100

    adding the register function in the AuthController class

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+const AuthService = require("../services/auth.service");
+
+class AuthController {
+  constructor() {
+    this.authService = new AuthService();
+  }
+  async register(req, res) {
+    try {
+      const user = await this.authService.register(req.body);
+      res.status(201).json({
+        success: true,
+        message: "Successfully registered.",
+        data: user._id,
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+}
+
+modules.exports = AuthController;
