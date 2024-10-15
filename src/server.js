514a83776b5609fa68afa4c3c5fe6d99e9cac518
AWEHA Nicolas <awenico98@gmail.com>
2024-10-15 18:35:04 +0100

    calling register route in server.js file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 require("./config/database");
 require("dotenv").config();
+const authRouter = require("./routes/auth.route");
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on http://localhost:${process.env.PORT}`)
