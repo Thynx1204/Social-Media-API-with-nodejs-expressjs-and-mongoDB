0c6299ab938f70b854e610254467a5ecc3413dfe
AWEHA Nicolas <awenico98@gmail.com>
2024-10-16 11:35:46 +0100

    feat: integrate Joi validation in AuthController register method

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,19 @@
 const AuthService = require("../services/auth.service");
 const authService = new AuthService();
+const userValidationSchema = require("../validation/user.validation")
 class AuthController {
   async register(req, res) {
     try {
+      const { error } = userValidationSchema.validate(req.body);
+
+      if (error) {
+        return res.status(400).json({
+          success: false,
+          message: error.details[0].message,
+        });
+      
+      }
+      
       const user = await authService.register(req.body);
       res.status(201).json({
         success: true,
