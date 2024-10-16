74e08d4674bd63f6815b47c05701a9608cf2cca1
AWEHA Nicolas <awenico98@gmail.com>
2024-10-16 18:11:13 +0100

    feat: add getUserById method to retrieve user by ID

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,6 +6,20 @@ class UserService {
     const users = await User.find().select("-password");
     return users;
   }
+
+  async getUserById(userId) {
+    if (!ObjectID.isValid(userId)) {
+      throw new Error("Invalid user ID");
+    }
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return user;
+  }
 }
 
 module.exports = UserService;
