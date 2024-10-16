b0542f88e91c26568d59fac3f6c986bc15ec9b93
AWEHA Nicolas <awenico98@gmail.com>
2024-10-16 12:16:44 +0100

    feat: add getAllUsers method to retrieve all users without passwords

---
+++ src/services/user.service..js
@@ -0,0 +1,9 @@
+const User = require("../models/user.model")
+const ObjectID = require("mongoose").Types.ObjectId;
+
+class UserService {
+    async getAllUsers () {
+        const users = await User.find().select("-password");
+        return users
+    }
+}
