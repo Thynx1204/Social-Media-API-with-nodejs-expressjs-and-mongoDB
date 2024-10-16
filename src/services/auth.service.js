7a9f8813844e526d8a4db8a61651ac5fb350191b
AWEHA Nicolas <awenico98@gmail.com>
2024-10-16 17:41:10 +0100

    feat: add validation to check for existing pseudo and email during registration

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,6 +4,19 @@ const { hashPassword } = require("../utils/password.util");
 class AuthService {
   async register(userData) {
     const { pseudo, email, password } = userData;
+
+    const existingUser = await User.findOne({
+      $or: [{ pseudo }, { email }],
+    });
+
+    if (existingUser) {
+      throw new Error(
+        existingUser.pseudo === pseudo
+          ? "Le pseudo est déjà pris"
+          : "L'e-mail est déjà enregistré"
+      );
+    }
+
     const user = await User.create({
       pseudo,
       email,
