b0a61d623dd730bb979515f9e2488e125def418a
AWEHA Nicolas <awenico98@gmail.com>
2024-10-17 11:08:01 +0100

    feat: Implemented updateUser method in UserController to handle user bio updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,6 +34,32 @@ class UserController {
       });
     }
   }
+
+  async updateUser(req, res) {
+    try {
+      const userId = req.params.id;
+      const userBio = req.body.bio;
+      const user = await userService.updateUser({ userId, userBio });
+
+      res.status(200).json({
+        success: true,
+        message: "User edit successfully",
+        data: user,
+      });
+    } catch (error) {
+      if (error.message === "Invalid user ID") {
+        return res.status(400).json({
+          success: false,
+          message: error.message,
+        });
+      }
+
+      res.status(404).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
 }
 
 module.exports = UserController;
