29a3433a0213d0d2183b0bc8c48a678306e3a61b
AWEHA Nicolas <awenico98@gmail.com>
2024-10-19 13:04:24 +0100

    feat: Implement token verification middleware for JWT authentication

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,48 @@
+const { verifyAccessToken } = require("../utils/jwt.util");
+
+function verifyToken(req, res, next) {
+  const authToken = req.headers.authorization;
+
+  if (!authToken || !authToken.startsWith("Bearer ")) {
+    res.status(403).json({
+      success: false,
+      message: "No token provided or invalid format.",
+    });
+    return;
+  }
+
+  const token = auth.split(" ")[1];
+
+  if (!token) {
+    res.status(401).json({
+      success: false,
+      message: "Token missing.",
+    });
+    return;
+  }
+
+  try {
+    const payload = verifyAccessToken(token);
+    req.user = payload._id;
+    next();
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token.",
+      });
+    } else if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Token has expired.",
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        message: "An unexpected error occurred.",
+      });
+    }
+  }
+}
+
+module.exports = verifyToken;
