8bcf5c2c2e31b6e4bc35b6fd246c9476bdac12c3
AWEHA Nicolas <awenico98@gmail.com>
2024-10-21 13:35:13 +0100

    feat: creating the postservice class with the createpost method

---
+++ src/services/post.service.js
@@ -0,0 +1,23 @@
+const Post = require("../models/post.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+class PostService {
+  async createPost(postData) {
+    const { posterId, message, picture, video, likers, comments } = postData;
+    if (!ObjectID.isValid(posterId)) {
+      throw new Error("Invalid user ID");
+    }
+    const post = await Post.create({
+      posterId,
+      message,
+      picture,
+      video,
+      likers,
+      comments,
+    });
+
+    return post;
+  }
+}
+
+module.exports = PostService;
