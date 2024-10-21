0b6ccf8bf64a334fa1e62a1e4daad768da5ef28f
AWEHA Nicolas <awenico98@gmail.com>
2024-10-21 14:21:32 +0100

    feat: added a set of routes for posts

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require("./config/database");
 require("dotenv").config();
 const authRouter = require("./routes/auth.route");
 const userRouter = require("./routes/user.route");
+const postRouter = require("./routes/post.route");
 const { verifyToken } = require("./middlewares/auth.middleware");
 
 const app = express();
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use("/auth", authRouter);
 
 app.use("/users", verifyToken, userRouter);
+
+app.use("post", verifyToken, postRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on http://localhost:${process.env.PORT}`)
