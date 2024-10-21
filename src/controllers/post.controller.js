0def94e1a6673a3dc47c36d06a68d0513edceb01
AWEHA Nicolas <awenico98@gmail.com>
2024-10-21 16:31:35 +0100

    feat: Add validation for creating a post using Joi

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,8 +1,19 @@
 const PostService = require("../services/post.service");
 const postService = new PostService();
+const postValidationSchema = require('../validation/post.validation')
 
 class PostController {
   async createPost(req, res) {
+    
+    const { error } = postValidationSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+
     const posterId = req.user.id;
     const { message, video } = req.body;
     const picture = "";
