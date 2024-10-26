42f3b9696de5d136bfc50a51d42cf88ce3245a6e
AWEHA Nicolas <awenico98@gmail.com>
2024-10-26 20:10:01 +0100

    fix: add module pathof nodejs

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,6 +1,7 @@
 const Post = require("../models/post.model");
 const User = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId;
+const path = require("path");
 require("dotenv").config()
 
 class PostService {
